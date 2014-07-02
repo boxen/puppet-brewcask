523e6440e58eb502bd915576304cc943c43761e5
Reinaldo Junior <juniorz@gmail.com>
2014-07-02 02:27:54 -0300

    Make sure cask is installed before installing any package it provides.
    Closes #7

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,27 @@
 class brewcask {
   require homebrew
 
-  homebrew::tap { 'caskroom/homebrew-cask': }
+  # caskroom is hardcoded to '/opt/homebrew-cask/Caskroom'
+  # https://github.com/caskroom/homebrew-cask/blob/master/lib/cask/locations.rb#L11
+  $cask_home = "/opt/homebrew-cask"
+  $cask_room = "${cask_home}/Caskroom"
+
+  homebrew::tap { 'caskroom/cask': }
+
+  file { $cask_home:
+    ensure => directory
+  }
+
+  # This prevents typing root password the first time a cask is installed
+  file { $cask_room:
+    ensure  => directory,
+    before  => Package['brew-cask'],
+    require => File[$cask_home]
+  }
 
   package { 'brew-cask':
-    require => Homebrew::Tap['caskroom/homebrew-cask']
+    require => Homebrew::Tap['caskroom/cask']
   }
+
+  Package['brew-cask'] -> Package <| provider == brewcask |>
 }
