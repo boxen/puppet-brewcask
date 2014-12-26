ac0ec9f5ab87957de5ac7a527b4df8374936622c
Jamie Connolly <jamie@jamieconnolly.com>
2014-12-26 12:36:02 +0000

    Flesh out the tests

--- spec/classes/brewcask_spec.rb
+++ spec/classes/brewcask_spec.rb
@@ -4,7 +4,13 @@ describe 'brewcask' do
   let(:facts) { default_test_facts }
 
   it do
+    should include_class('homebrew')
+
     should contain_homebrew__tap('caskroom/cask')
-    should contain_package('brew-cask')
+
+    should contain_file('/opt/homebrew-cask').with_ensure('directory')
+    should contain_file('/opt/homebrew-cask/Caskroom').with_ensure('directory')
+
+    should contain_package('brew-cask').with_provider('homebrew')
   end
 end
