52b565fb365cbae307815b5a2e6a528f10970b33
Radek Simko <radek.simko@gmail.com>
2015-02-24 08:17:49 +0000

    Tests decoupled into smaller chunks
    This will allow more granular progress reporting during runs.

--- spec/classes/brewcask_spec.rb
+++ spec/classes/brewcask_spec.rb
@@ -5,14 +5,19 @@ describe 'brewcask' do
 
   it do
     should contain_class('homebrew')
-
     should contain_homebrew__tap('caskroom/cask')
+  end
 
+  it do
     should contain_file('/test/custom/homebrew-cask').with_ensure('directory')
     should contain_file('/test/custom/homebrew-cask/Caskroom').with_ensure('directory')
+  end
 
+  it do
     should contain_package('brew-cask').with_provider('homebrew')
+  end
 
+  it do
     should contain_boxen__env_script('brewcask').with_ensure('present')
   end
 end
