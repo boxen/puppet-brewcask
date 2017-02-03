726be32f1c51f637fe707c4dbbeda38bf91cc824
Jacob Bednarz <jacob.bednarz@gmail.com>
2017-02-03 14:32:17 +1100

    Remove all puppet specs

--- spec/classes/brewcask_spec.rb
+++ spec/classes/brewcask_spec.rb
@@ -2,18 +2,4 @@ require 'spec_helper'
 
 describe 'brewcask' do
   let(:facts) { default_test_facts }
-
-  it do
-    should contain_class('homebrew')
-    should contain_homebrew__tap('caskroom/cask')
-  end
-
-  it do
-    should contain_file('/test/custom/homebrew-cask').with_ensure('directory')
-    should contain_file('/test/custom/homebrew-cask/Caskroom').with_ensure('directory')
-  end
-
-  it do
-    should contain_boxen__env_script('brewcask').with_ensure('present')
-  end
 end
